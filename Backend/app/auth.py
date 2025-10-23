caa77de8b2287f1297624edcba81da3685933a9f
PedroPassos87 <peagacarreira@gmail.com>
2025-10-23 03:51:18 -0300

    refactor following frontend json

--- Backend/app/auth.py
+++ Backend/app/auth.py
@@ -1,29 +1,24 @@
+from fastapi.responses import JSONResponse
 from typing_extensions import Annotated
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends
 from passlib.context import CryptContext
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
-from pydantic import BaseModel
-from User.userBase import UserResponse
+from fastapi.security import  OAuth2PasswordBearer
+from User.userBase import LoginRequest, UserResponseLogin, ResponseDataLogin, UserDataLogin
 from database import SessionLocal
 from models import User
 from datetime import timedelta, datetime
-from jose import jwt, JWTError
+from jose import jwt
 from keys import ALGORITHM, SECRET_KEY
 from starlette import status
 
 
 router = APIRouter(
-    prefix='/auth',
+    prefix='/api/auth',
     tags=['auth']
 )
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated='auto')
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-    user: UserResponse
     
 def get_db():
     db = SessionLocal()
@@ -42,42 +37,29 @@ def authenticate_user(email: str, password: str, db):
         return False
     return user
 
-def create_access_token(name: str, user_id: int, expires_delta: timedelta):
-    encode = {"sub": name, "id": user_id}
-    expires = datetime.timezone.utc() + expires_delta
+def create_access_token(email: str, user_id: int, expires_delta: timedelta):
+    encode = {"sub": email, "id": user_id}
+    expires = datetime.now(datetime.timezone.utc) + expires_delta
     encode.update({"exp": expires})
     return jwt.encode(encode, SECRET_KEY, ALGORITHM)
 
-@router.post("/token", response_model=Token)
-async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
-    user = authenticate_user(form_data.username, form_data.password, db)
+@router.post("/login", response_model=UserResponseLogin)
+async def login_for_access_token(login_data: LoginRequest, db: db_dependency):
+    user = authenticate_user(login_data.email, login_data.password, db)
     if not user:
-        raise HTTPException(
+        return JSONResponse(
             status_code= status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
+            content={
+                "status": "error",
+                "message": "Credenciais inválidas",
+                "code": 401
+            })
+    token = create_access_token(user.email, user.id,  timedelta(days=3))
+    return UserResponseLogin(
+        status="success",
+        message="Login realizado com sucesso",
+        data=ResponseDataLogin(
+            token=token,
+            usuario=UserDataLogin.model_validate()(user)
         )
-    token = create_access_token(user.email, user.id, timedelta(days=3))
-    return {"access_token": token,"token_type": "bearer","user": user}
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)], db: db_dependency):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        name: str = payload.get("sub")
-        user_id: int = payload.get("id")
-        if name is None or user_id is None:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Could not validate credentials",
-            )
-    except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-        )
-    user = db.query(User).filter(User.id == user_id).first()
-    if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Could not validate credentials",
-        )
-    return user
+    )
