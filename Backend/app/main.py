0df9a3243b3dee1c9395d67aefcb8c5c92f41a08
PedroPassos87 <peagacarreira@gmail.com>
2025-10-23 03:51:44 -0300

    refactor

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
 import uvicorn
 
-from .routers import teste
-
 app = FastAPI()
 
-app.include_router(teste.router, prefix="/api/v1", tags=["teste"])
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
