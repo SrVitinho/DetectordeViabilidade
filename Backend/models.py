397c7b77fdae4b835e0a1b1e94b7593408cfb7b0
PedroPassos87 <peagacarreira@gmail.com>
2025-11-01 13:16:36 -0300

    feat: analysis model

--- Backend/models.py
+++ Backend/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, JSON, ForeignKey
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 
 from database import Base
 
@@ -12,6 +13,23 @@ class User(Base):
     phone = Column(String(15), nullable=True)
     company = Column(String(128), nullable=True)
     password = Column(String(300), nullable=False)
+    
+    data_criacao = Column(
+        DateTime(timezone=True), 
+        server_default=func.now()
+    )
+    
+    analyses = relationship("Analysis", back_populates="owner")
+    
+class Analysis(Base):
+    __tablename__ = "Analysis"
+    
+    id = Column(Integer, primary_key=True, index=True)    
+    owner_id = Column(Integer, ForeignKey("User.id"))
+    owner = relationship("User", back_populates="analyses")
+    
+    request_data = Column(JSON, nullable=False)
+    result_data = Column(JSON, nullable=False)
     
     data_criacao = Column(
         DateTime(timezone=True), 
